b115b6063bed3f7465c748eb2cb666e01850d11e
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-26 02:27:46 +0100

    Fix bugs with deploy script
    Fix minor bugs with deploy script
    
    Fix minor bugs
    
    Fixes
    
    Fixes
    
    Fix

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -1,19 +1,18 @@
-import execa from "execa";
-import { existsSync } from "fs";
+import { execa } from "execa";
+
 (async () => {
     try {
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building...");
-        await execa("yarn", "build");
+        await execa("yarn", ["run", "build"]);
         // Understand if it's dist or build folder
-        const folderName = existsSync("dist") ? "dist" : "build";
-        await execa("git", ["--work-tree", folderName, "add", "--all"]);
-        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        await execa("git", ["--work-tree", "dist", "add", "--all"]);
+        await execa("git", ["--work-tree", "dist", "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-        await execa("rm", ["-r", folderName]);
-        await execa("git", ["checkout", "-f", "master"]);
+        await execa("rm", ["-r", "dist"]);
+        await execa("git", ["checkout", "-f", "main"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed.");
     } catch (e) {
