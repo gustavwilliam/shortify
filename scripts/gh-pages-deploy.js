ad0b40f9dc4fec3204e3ed80e2a4d001d8306f8d
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-26 02:07:26 +0100

    Add deploy script

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,24 @@
+import execa from "execa";
+import { existsSync } from "fs";
+(async () => {
+    try {
+        await execa("git", ["checkout", "--orphan", "gh-pages"]);
+        // eslint-disable-next-line no-console
+        console.log("Building...");
+        await execa("yarn", "build");
+        // Understand if it's dist or build folder
+        const folderName = existsSync("dist") ? "dist" : "build";
+        await execa("git", ["--work-tree", folderName, "add", "--all"]);
+        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        console.log("Pushing to gh-pages...");
+        await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+        await execa("rm", ["-r", folderName]);
+        await execa("git", ["checkout", "-f", "master"]);
+        await execa("git", ["branch", "-D", "gh-pages"]);
+        console.log("Successfully deployed.");
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e.message);
+        process.exit(1);
+    }
+})();
