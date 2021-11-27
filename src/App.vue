8dea02f1a2c8a43966c055a9c95ecedea41dcd0d
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-27 22:49:00 +0100

    Add base for LinkCreator component

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@
       { name: 'GitHub', href: 'https://gh.godi.se/shortify', current: false },
     ]"
   />
-  <div></div>
+  <div>
+    <div
+      class="max-w-2xl mx-auto py-12 px-4 sm:py-20 sm:px-6 lg:max-w-7xl lg:px-8"
+    >
+      <LinkCreator />
+    </div>
+  </div>
   <TheFooter />
 </template>
 
@@ -13,9 +19,10 @@
 import { defineComponent } from "vue";
 import TheNavbar from "@/components/TheNavbar.vue";
 import TheFooter from "@/components/TheFooter.vue";
+import LinkCreator from "@/components/LinkCreator/LinkCreator.vue";
 
 export default defineComponent({
   name: "App",
-  components: { TheNavbar, TheFooter },
+  components: { TheNavbar, TheFooter, LinkCreator },
 });
 </script>
---
+++ src/components/LinkCreator/LinkCreator.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable-next-line vue/valid-template-root -->
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
