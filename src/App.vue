85aa0338bb9cf9c30315bfefc616070f805f344c
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-11-27 15:31:12 +0100

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,17 @@
       { name: 'GitHub', href: 'https://gh.godi.se/shortify', current: false },
     ]"
   />
+  <div></div>
+  <TheFooter />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import TheNavbar from "@/components/TheNavbar.vue";
+import TheFooter from "@/components/TheFooter.vue";
 
 export default defineComponent({
   name: "App",
-  components: { TheNavbar },
+  components: { TheNavbar, TheFooter },
 });
 </script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,69 @@
+<template>
+  <footer class="text-gray-300 bg-gray-800 bottom-0">
+    <div
+      class="
+        grid
+        gap-8
+        sm:grid-cols-4
+        max-w-2xl
+        mx-auto
+        py-12
+        px-4
+        sm:px-6
+        lg:max-w-7xl lg:px-8
+      "
+    >
+      <div>
+        <img
+          class="h-8"
+          src="@/assets/logo/logo-dark-white-text.svg"
+          alt="Shortify logotype"
+        />
+      </div>
+      <div class="text-sm col-span-2">
+        Free, open source link shortener! Thanks to
+        <a
+          href="https://github.com/gustavwilliam/"
+          target="_blank"
+          class="underline hover:text-gray-50"
+          >Gustav Odinger</a
+        >
+        for creating this site.
+      </div>
+      <div class="flex flex-col items-start sm:items-end max-w-screen-xl">
+        <div class="flex items-center space-x-2">
+          <a
+            href="https://github.com/gustavwilliam"
+            target="_blank"
+            class="hover:text-gray-50 flex flex-row space-x-3"
+          >
+            <img
+              class="w-6 h-6"
+              aria-hidden="true"
+              src="@/assets/social-icons/github.svg"
+              alt="GitHub logo"
+            />
+            <span class="sr-only">GitHub</span>
+          </a>
+          <a
+            href="https://twitter.com/gustavodinger"
+            target="_blank"
+            class="hover:text-gray-50 flex flex-row space-x-3"
+          >
+            <img
+              class="w-6 h-6"
+              aria-hidden="true"
+              src="@/assets/social-icons/twitter.svg"
+              alt="Twitter logo"
+            />
+            <span class="sr-only">Twitter</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
