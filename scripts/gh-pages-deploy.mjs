d6aa38410781d4d1cd66cd85926726e106e72979
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-01-31 20:26:47 +0100

    Please the 2-space JS gods

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -1,23 +1,23 @@
 import { execa } from "execa";
 
 (async () => {
-    try {
-        await execa("git", ["checkout", "--orphan", "gh-pages"]);
-        // eslint-disable-next-line no-console
-        console.log("Building...");
-        await execa("yarn", ["run", "build"]);
-        // Understand if it's dist or build folder
-        await execa("git", ["--work-tree", "dist", "add", "--all"]);
-        await execa("git", ["--work-tree", "dist", "commit", "-m", "gh-pages"]);
-        console.log("Pushing to gh-pages...");
-        await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-        await execa("rm", ["-r", "dist"]);
-        await execa("git", ["checkout", "-f", "main"]);
-        await execa("git", ["branch", "-D", "gh-pages"]);
-        console.log("Successfully deployed.");
-    } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log(e.message);
-        process.exit(1);
-    }
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building...");
+    await execa("yarn", ["run", "build"]);
+    // Understand if it's dist or build folder
+    await execa("git", ["--work-tree", "dist", "add", "--all"]);
+    await execa("git", ["--work-tree", "dist", "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", "dist"]);
+    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed.");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
 })();
