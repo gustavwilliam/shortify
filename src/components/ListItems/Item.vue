1bb25c1386ac16976a9a01b5ee7467157b9ac63d
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-02-06 01:49:38 +0100

    Ensure line breaks on list page on mobile
    Links got too long and scaled the page horizontally. This is fixed by
    forcing line breaks

--- src/components/ListItems/Item.vue
+++ src/components/ListItems/Item.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="even:bg-white dark:even:bg-gray-800 odd:bg-gray-50 dark:odd:bg-gray-750 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+    class="even:bg-white break-all dark:even:bg-gray-800 odd:bg-gray-50 dark:odd:bg-gray-750 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
   >
     <dt class="text-sm font-medium text-gray-500 dark:text-gray-300">
       {{ short }}
